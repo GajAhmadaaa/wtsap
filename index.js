fc38ebbf4dcd6a921ed373c29edb558da6809007
GajAhmadaaa <64576446+GajAhmadaaa@users.noreply.github.com>
2021-03-05 14:00:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const wa = require("@open-wa/wa-automate");
-const { create, decryptMedia, ev } = wa;
+const { create, decryptMedia, ev } = require("@open-wa/wa-automate");
 const { default: PQueue } = require("p-queue");
 const fs = require("fs");
 const express = require("express");
 const axios = require("axios").default;
-
-const helpOnInPM = ["hello", "hi", "hii", "hey", "heyy", "#help", "#menu"];
-const helpOnInGroup = ["#help", "#menu"];
 
 const configObject = {
   sessionId: "SAD_CLIENT",
@@ -20,41 +16,7 @@ const configObject = {
   qrTimeout: 0,
 };
 
-const helpText =
-  process.env.HELP_TEXT ||
-  `Commands:
-#sticker: write in caption of a image/video/gif to turn it into sticker
-#spam: tag everyone in a message in a group (only works in a group)
-#join https://chat.whatsapp.com/shdkashdh: joing a group with invite link
-#leave: i hope you dont use this (only works in a group if sent by an admin)
-#help: to recive this same message
-#menu: same as help but some people prefer it
-#run languages: Returns all languages supported
-#run {language}
-{code}: Run some code in some language
-eg.
-'#run node
-console.log('hello world');'
-Add '#nospam' in group description to stop spam commands
-All commands except #spam & #leave work in pm
-Made by: pathetic_geek (https://github.com/patheticGeek)
-`;
-
-const leaveText =
-  process.env.LEAVE_TEXT ||
-  "Ab unko humshe rishta nhi rakhna hai\nto humari taraf se bhi koi zabardasti nhi hai";
-
-/**
- * WA Client
- * @type {null | import("@open-wa/wa-automate").Client}
- */
-let cl = null;
-
-/**
- * Process the message
- * @param {import("@open-wa/wa-automate").Message} message
- */
-async function procMess(message) {
+const startBot = async () => {
  try {
     const Handler = require("./handler");
     const Client = await create(configObject);
@@ -80,33 +42,11 @@ async function procMess(message) {
 
 const server = express();
 const PORT = parseInt(process.env.PORT) || 3000;
-const queue = new PQueue({
-  concurrency: 2,
-  autoStart: false,
-});
-/**
- * Add message to process queue
- */
-const processMessage = (message) =>
-  queue.add(async () => {
-    try {
-      await procMess(message);
-    } catch (e) {
-      console.log(e);
-    }
-  });
 
 /**
  * Initialize client
  * @param {import("@open-wa/wa-automate").Client} client
  */
-async function start(client) {
-  cl = client;
-  queue.start();
-  const unreadMessages = await client.getAllUnreadMessages();
-  unreadMessages.forEach(processMessage);
-  client.onMessage(processMessage);
-}
 
 ev.on("qr.**", async (qrcode) => {
   const imageBuffer = Buffer.from(
@@ -115,11 +55,6 @@ ev.on("qr.**", async (qrcode) => {
   );
   fs.writeFileSync("./public/qr_code.png", imageBuffer);
 });
-
-create({
-  qrTimeout: 0,
-  cacheEnabled: false,
-}).then((client) => start(client));
 
 server.use(express.static("public"));
 server.listen(PORT, () => 
@@ -131,3 +66,5 @@ process.on("exit", () => {
     fs.unlinkSync("./session.data.json");
   }
 });
+
+startBot();
